19a6e635cdc80338f7ffb68c9e7cfa44e865a2c5
JMckinty96 <Jamesmckinty@gmail.com>
2020-04-17 05:18:47 +0100

    Collision works

--- CrimsonDungeon/js/game.js
+++ CrimsonDungeon/js/game.js
@@ -136,8 +136,22 @@ function create() {
     //    repeat: -1
     //});
 
+    // Debug graphics
+    this.input.keyboard.once("keydown_D", event => {
+        // Turn on physics debugging to show player's hitbox
+        this.physics.world.createDebugGraphic();
 
-
+        // Create worldLayer collision graphic above the player, but below the help text
+        const graphics = this.add
+            .graphics()
+            .setAlpha(0.75)
+            .setDepth(20);
+        wallLayer.renderDebug(graphics, {
+            tileColor: null, // Color of non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        });
+    });
 
     // Help text that has a "fixed" position on the screen
     this.add
